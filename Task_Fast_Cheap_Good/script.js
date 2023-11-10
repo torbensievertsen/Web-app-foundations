cc54a3808f0fd9b929b44a80fda9b3f62334be87
Torben Sievertsen <torben.sievertsen27@gmail.com>
2023-11-10 10:34:52 +0100

    finished Fast-Cheap-Good

---
+++ Task_Fast_Cheap_Good/script.js
@@ -0,0 +1,45 @@
+//wenn eine checkbox true gesetzt wird überprüfen wie viele sonst true sind wenn 3 dann remove last true
+const Fast = document.querySelector("#fast");
+const Cheap = document.querySelector("#cheap");
+const Good = document.querySelector("#good");
+let test = 0;
+
+Fast.addEventListener("click", function () {
+  if (Fast.checked) {
+    if (Cheap.checked && Good.checked) {
+      if (test === 2) {
+        Cheap.checked = false;
+      }
+      if (test === 3) {
+        Good.checked = false;
+      }
+    }
+  }
+  test = 1;
+});
+Cheap.addEventListener("click", function () {
+  if (Cheap.checked) {
+    if (Fast.checked && Good.checked) {
+      if (test === 1) {
+        Fast.checked = false;
+      }
+      if (test === 3) {
+        Good.checked = false;
+      }
+    }
+  }
+  test = 2;
+});
+Good.addEventListener("click", function () {
+  if (Good.checked) {
+    if (Cheap.checked && Good.checked) {
+      if (test === 2) {
+        Cheap.checked = false;
+      }
+      if (test === 1) {
+        Fast.checked = false;
+      }
+    }
+  }
+  test = 3;
+});
